8ddfb5ec9cb89a63758aefd8293c082107c607b9
LiLCraFT <olive.lucas.pro@gmail.com>
2026-06-08 00:07:04 +0200

    feat: integrate UrgencyBanner and FloatingCallButton in Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,16 +2,20 @@ import { Outlet } from 'react-router-dom'
 import Navbar from './Navbar'
 import Footer from './Footer'
 import CtaBanner from './CtaBanner'
+import UrgencyBanner from './UrgencyBanner'
+import FloatingCallButton from './FloatingCallButton'
 
 export default function Layout() {
   return (
     <div className="flex min-h-screen flex-col">
+      <UrgencyBanner />
       <Navbar />
       <main className="flex-1">
         <Outlet />
       </main>
       <Footer />
       <CtaBanner />
+      <FloatingCallButton />
       <div className="h-16 md:hidden" />
     </div>
   )
